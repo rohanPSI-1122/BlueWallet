86debf49da11ffa7b86f1dcbfeb6ad699bbf5630
rohanPSI-1122 <rgaddam@primesoft.net>
2025-07-09 15:34:08 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,11 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {
-  resolver: {
-    blockList: exclusionList([
-      /current-branch\.json$/, // 👈 Ignore this file
-    ]),
-  },
-};
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
